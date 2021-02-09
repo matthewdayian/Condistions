a8a3244d1f52bb616064ca212db5ddf6f4d2eff5
Matthew <Matthew@localhost>
2021-02-09 09:07:29 -0500

    ex5

---
+++ Conditions/src/Ex5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+/*
+* Author : Matthew
+* Date : Feb. 9, 2021
+*/
+public class Ex5 {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println ("Quelle est la resultat de ta premiere jeux");
+		System.out.println ("Quelle est la resultat de ta deuxieme jeux");
+		System.out.println ("Quelle est la resultat de ta troisieme jeux");
+		
+	}
+
+}
