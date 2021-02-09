69fd584a4c9109a616a1e0ab4392d27acf95894e
Matthew <Matthew@localhost>
2021-02-09 09:08:01 -0500

    ex5

--- Conditions/src/Ex5.java
+++ Conditions/src/Ex5.java
@@ -8,9 +8,20 @@ public class Ex5 {
 	public static void main(String[] args) 
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println ("Quelle est la resultat de ta premiere jeux");
-		System.out.println ("Quelle est la resultat de ta deuxieme jeux");
-		System.out.println ("Quelle est la resultat de ta troisieme jeux");
+		System.out.println ("Combien de jeux est-ce que tu as gagner dans tes 6 premiere jeux");
+		int victoires=sc.nextInt();
+		
+		if (victoires>=5)
+			System.out.println("group 1");
+		else
+			if (3<=victoires<=4)
+				System.out.println("group 2");
+			else
+				if (victoires<=2)
+					System.out.println("group 3");
+				else
+					if (victoires==0)
+						System.out.println("eliminer");
 		
 	}
 
