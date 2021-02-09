ba9e85670d401f255c0d4cdcf2adf8432beedaba
Matthew <Matthew@localhost>
2021-02-09 08:53:23 -0500

    ex2

--- Conditions/src/EX2.java
+++ Conditions/src/EX2.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
 /*
 * Author : Matthew
 * Date : Feb. 5, 2021
 */
 public class EX2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Quelle est le nombre d'antennas");
+		int antenna = sc.nextInt();
+		System.out.println("Quelle est le nombre d'oeil");
+		int oeil = sc.nextInt();
+		
+		if (antenna>3;oeil<4)
+			System.out.println("TroyMartian");
+		else
+			if (antenna<6;oeil>2)
+				System.out.println("VladSaturian");
+			else
+				if (antenna<2;oeil<3)
+					System.out.println("GraemeMercurian");
+				
 
 	}
 
